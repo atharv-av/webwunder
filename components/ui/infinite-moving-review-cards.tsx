d18b89483b50f09ec7c3bfb0d5c5167508cab5bb
axat0207 <axxatagrawal@gmail.com>
2024-09-26 08:11:01 +0530

    infinite review shadow

--- components/ui/infinite-moving-review-cards.tsx
+++ components/ui/infinite-moving-review-cards.tsx
@@ -3,6 +3,7 @@
 import { cn } from '@/lib/utils'
 import { Star } from 'lucide-react'
 import React, { useEffect, useState, useRef } from 'react'
+import Marquee from 'react-fast-marquee'
 
 export const InfiniteMovingCards = ({
     items,
@@ -23,6 +24,31 @@ export const InfiniteMovingCards = ({
 }) => {
     const containerRef = useRef<HTMLDivElement>(null)
     const scrollerRef = useRef<HTMLUListElement>(null)
+
+    const [scale, setScale] = useState('')
+
+    useEffect(() => {
+        const handleResize = () => {
+            const windowWidth = window.innerWidth
+
+            // Define breakpoints at intervals of 100px
+            if (windowWidth >= 1536) {
+                setScale('max-w-[1700px]') // For 1536px and above
+            } else if (windowWidth >= 1280) {
+                setScale('max-w-[1700px]') // For 1400px to 1535px
+            } else if (windowWidth >= 1024) {
+                setScale('max-w-[1300px]') // For 1300px to 1399px
+            } else {
+                setScale('max-w-[1300px]') // Default or fallback width
+            }
+        }
+
+        // Call the function on mount and when window is resized
+        window.addEventListener('resize', handleResize)
+        handleResize() // Call it once to set the initial width
+
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
 
     useEffect(() => {
         setAnimationProperties()
@@ -73,128 +99,129 @@ export const InfiniteMovingCards = ({
     }
 
     return (
-        <div className="relative overflow-hidden">
-    {/* Left Gradient */}
-    <div className="absolute inset-y-0 left-0 w-16 bg-gradient-to-r from-black/80 to-transparent pointer-events-none z-10"></div>
-
-    {/* Right Gradient */}
-    <div className="absolute inset-y-0 right-0 w-16 bg-gradient-to-l from-black/80 to-transparent pointer-events-none z-10"></div>
-
-    {/* Scrolling List */}
-    <ul
-        ref={scrollerRef}
-        className={cn(
-            'flex w-max animate-scroll flex-nowrap gap-4 py-4 z-0',
-            start && 'start-animation',
-            pauseOnHover && 'hover:[animation-play-state:paused]',
-        )}
-    >
-        {items.map((item, idx) => (
-            <li
-                className="relative w-[350px] max-w-full flex-shrink-0 rounded-2xl border border-b-0 border-slate-700 px-8 py-10 md:w-[500px]"
-                style={{
-                    background:
-                        'linear-gradient(180deg, var(--slate-800), var(--slate-900))',
-                }}
-                key={`${item.writer}-${idx}`}
+        <div
+            ref={containerRef}
+            className={`scroller relative z-20 max-w-[1400px] overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)] xl:max-w-[1700px] xl:[mask-image:linear-gradient(to_right,transparent,white_10%,white_90%,transparent)]`}
+        >
+            <ul
+                ref={scrollerRef}
+                className={cn(
+                    'flex w-max animate-scroll flex-nowrap gap-4 py-4',
+                    start && 'start-animation',
+                    pauseOnHover && 'hover:[animation-play-state:paused]',
+                )}
             >
-                <blockquote>
-                    <div
-                        aria-hidden="true"
-                        className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%+_4px)] w-[calc(100%+_4px)] rounded-xl bg-[#191919]"
-                    ></div>
-                    <span className="relative z-20 mb-1 font-figtree text-[22px] font-semibold leading-[1.6] text-white">
-                        &quot;{item.content}&quot;
-                    </span>
-                    <div className="relative z-20 mt-6 flex flex-col items-start">
-                        <span className="mb-3 font-figtree text-sm font-normal leading-[1.6] text-white/50">
-                            {item.writer}
-                        </span>
-
-                        <div className="flex flex-row items-center">
-                            {[...Array(Math.floor(item.rating))].map(
-                                (_, i) => (
-                                    <span
-                                        key={i}
-                                        className="text-[#FFDC26]"
-                                    >
-                                        <Star size={20} fill="#FFDC26" />
-                                    </span>
-                                ),
-                            )}
-                            {item.rating % 1 !== 0 && (
-                                <span className="text-[#FFDC26]">
-                                    <Star size={20} fill="#FFDC26" />
+                {items.map((item, idx) => (
+                    <li
+                        className="relative w-[350px] max-w-full flex-shrink-0 rounded-2xl border border-b-0 border-slate-700 px-8 py-10 md:w-[500px]"
+                        style={{
+                            background:
+                                'linear-gradient(180deg, var(--slate-800), var(--slate-900))',
+                        }}
+                        key={`${item.writer}-${idx}`}
+                    >
+                        <blockquote>
+                            <div
+                                aria-hidden="true"
+                                className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%+_4px)] w-[calc(100%+_4px)] rounded-xl bg-[#191919]"
+                            ></div>
+                            <span className="relative z-20 mb-1 font-figtree text-[22px] font-semibold leading-[1.6] text-white">
+                                &quot;{item.content}&quot;
+                            </span>
+                            <div className="relative z-20 mt-6 flex flex-col items-start">
+                                <span className="mb-3 font-figtree text-sm font-normal leading-[1.6] text-white/50">
+                                    {item.writer}
                                 </span>
-                            )}
-                        </div>
-                    </div>
-                </blockquote>
-            </li>
-        ))}
-        {/* Duplicate the list to create the illusion of infinite scrolling */}
-        {items.map((item, idx) => (
-            <li
-                className="relative w-[350px] max-w-full flex-shrink-0 rounded-2xl border border-b-0 border-slate-700 px-8 py-10 md:w-[500px]"
-                style={{
-                    background:
-                        'linear-gradient(180deg, var(--slate-800), var(--slate-900))',
-                }}
-                key={`duplicate-${item.writer}-${idx}`}
-            >
-                <blockquote>
-                    <div
-                        aria-hidden="true"
-                        className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%+_4px)] w-[calc(100%+_4px)] rounded-xl bg-[#191919]"
-                    ></div>
-                    <span className="relative z-20 mb-1 font-figtree text-[22px] font-semibold leading-[1.6] text-white">
-                        &quot;{item.content}&quot;
-                    </span>
-                    <div className="relative z-20 mt-6 flex flex-col items-start">
-                        <span className="mb-3 font-figtree text-sm font-normal leading-[1.6] text-white/50">
-                            {item.writer}
-                        </span>
 
-                        <div className="flex flex-row items-center">
-                            {[...Array(Math.floor(item.rating))].map(
-                                (_, i) => (
-                                    <span
-                                        key={i}
-                                        className="text-[#FFDC26]"
-                                    >
-                                        <Star size={20} fill="#FFDC26" />
-                                    </span>
-                                ),
-                            )}
-                            {item.rating % 1 !== 0 && (
-                                <span className="text-[#FFDC26]">
-                                    <Star size={20} fill="#FFDC26" />
+                                <div className="flex flex-row items-center">
+                                    {[...Array(Math.floor(item.rating))].map(
+                                        (_, i) => (
+                                            <span
+                                                key={i}
+                                                className="text-[#FFDC26]"
+                                            >
+                                                <Star
+                                                    size={20}
+                                                    fill="#FFDC26"
+                                                />
+                                            </span>
+                                        ),
+                                    )}
+                                    {item.rating % 1 !== 0 && (
+                                        <span className="text-[#FFDC26]">
+                                            <Star size={20} fill="#FFDC26" />
+                                        </span>
+                                    )}
+                                </div>
+                            </div>
+                        </blockquote>
+                    </li>
+                ))}
+                {/* Duplicate the list to create the illusion of infinite scrolling */}
+                {items.map((item, idx) => (
+                    <li
+                        className="relative w-[350px] max-w-full flex-shrink-0 rounded-2xl border border-b-0 border-slate-700 px-8 py-10 md:w-[500px]"
+                        style={{
+                            background:
+                                'linear-gradient(180deg, var(--slate-800), var(--slate-900))',
+                        }}
+                        key={`duplicate-${item.writer}-${idx}`}
+                    >
+                        <blockquote>
+                            <div
+                                aria-hidden="true"
+                                className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%+_4px)] w-[calc(100%+_4px)] rounded-xl bg-[#191919]"
+                            ></div>
+                            <span className="relative z-20 mb-1 font-figtree text-[22px] font-semibold leading-[1.6] text-white">
+                                &quot;{item.content}&quot;
+                            </span>
+                            <div className="relative z-20 mt-6 flex flex-col items-start">
+                                <span className="mb-3 font-figtree text-sm font-normal leading-[1.6] text-white/50">
+                                    {item.writer}
                                 </span>
-                            )}
-                        </div>
-                    </div>
-                </blockquote>
-            </li>
-        ))}
-    </ul>
 
-    {/* CSS for Infinite Scroll Animation */}
-    <style jsx>{`
-        @keyframes scroll {
-            0% {
-                transform: translateX(0);
-            }
-            100% {
-                transform: translateX(calc(-100% / 2));
-            }
-        }
+                                <div className="flex flex-row items-center">
+                                    {[...Array(Math.floor(item.rating))].map(
+                                        (_, i) => (
+                                            <span
+                                                key={i}
+                                                className="text-[#FFDC26]"
+                                            >
+                                                <Star
+                                                    size={20}
+                                                    fill="#FFDC26"
+                                                />
+                                            </span>
+                                        ),
+                                    )}
+                                    {item.rating % 1 !== 0 && (
+                                        <span className="text-[#FFDC26]">
+                                            <Star size={20} fill="#FFDC26" />
+                                        </span>
+                                    )}
+                                </div>
+                            </div>
+                        </blockquote>
+                    </li>
+                ))}
+            </ul>
 
-        .animate-scroll {
-            animation: scroll 30s linear infinite;
-            animation-direction: normal;
-        }
-    `}</style>
-</div>
+            {/* CSS for Infinite Scroll Animation */}
+            <style jsx>{`
+                @keyframes scroll {
+                    0% {
+                        transform: translateX(0);
+                    }
+                    100% {
+                        transform: translateX(calc(-100% / 2));
+                    }
+                }
 
+                .animate-scroll {
+                    animation: scroll var(--animation-duration) linear infinite;
+                    animation-direction: var(--animation-direction);
+                }
+            `}</style>
+        </div>
     )
 }
